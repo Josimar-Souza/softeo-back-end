45b3e3a7567d4f7a5242a2fb97d0e775854f1256
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-26 17:14:54 -0300

    Created app and server files

---
+++ server/server.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config();
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
