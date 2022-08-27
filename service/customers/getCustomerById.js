2a6dc225a9182eae404627987d503f19ab7a7444
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-27 15:13:05 -0300

    Created getCustomerById service

---
+++ service/customers/getCustomerById.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require('http-status-codes');
+const customersModel = require('../../model/customers');
+const CustomError = require('../../helpers/customError');
+
+module.exports = async (id) => {
+  const customer = await customersModel.getCustomerById(id);
+
+  if (!customer) {
+    return new CustomError('Customer not found!', StatusCodes.NOT_FOUND);
+  }
+
+  return customer;
+};
