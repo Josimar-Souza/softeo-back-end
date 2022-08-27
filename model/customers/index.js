42427bc952df32823a3521946499426435a29b6a
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-27 16:38:31 -0300

    Created updateCustomerById model

--- model/customers/index.js
+++ model/customers/index.js
@@ -2,12 +2,14 @@ const addCustomer = require('./addCustomer');
 const getAllCustomers = require('./getAllCustomers');
 const getCustomerById = require('./getCustomerById');
 const deleteCustomerById = require('./deleteCustomerById');
+const updateCustomerById = require('./updateCustomerById');
 
 const customersModel = {
   addCustomer,
   getAllCustomers,
   getCustomerById,
   deleteCustomerById,
+  updateCustomerById,
 };
 
 module.exports = customersModel;
