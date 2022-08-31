5ae6bef571630b79df3adc8b8e3d3b5f70af43f5
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-31 15:18:42 -0300

    Added cors to the app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const rootRouter = require('../controller/rootRouter');
 const Error = require('../middlewares/Error');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 rootRouter(app);
 
